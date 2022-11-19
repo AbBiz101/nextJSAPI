33a3b07af086c8ea83d92db3e39e51dd338e3337
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-19 14:59:10 +0100

    Update feedback.js

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -2,26 +2,25 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-
 	if (req.method === 'POST') {
 		const email = req.body.email;
 		const feedback = req.body.feedback;
 		const newFeedback = {
-			id: new Date().toISOString,
+			id: new Date().toISOString(),
 			email: email,
 			feedback: feedback,
-    };
-    
+		};
+
 		const feedbackFile = path.join(process.cwd(), 'data', 'feedback.json');
 		const oldData = fs.readFileSync(feedbackFile);
 		const data = JSON.parse(oldData);
-		data.Push(newFeedback);
-    fs.writeFileSync(filePath, JSON.stringify(data));
-    
+		data.push(newFeedback);
+		fs.writeFileSync(feedbackFile, JSON.stringify(data));
+
 		res
 			.status(200)
 			.json({ message: 'Feedback added successfully!', feedback: newFeedback });
-  }
-  
+	}
+
 	res.status(200).json({ message: 'This works just fine!' });
 }
