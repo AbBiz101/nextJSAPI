0fac924b1c18685cbb5da1d485a78053cd648354
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-19 14:34:30 +0100

    Update feedback.js

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,3 +1,15 @@
 export default function handler(req, res) {
+	if (req.method === 'POST') {
+		const email = req.body.email;
+    const feedback = req.body.feedback;
+    
+		const newFeedback = {
+			id: new Date().toISOString,
+			email: email,
+			feedback: feedback,
+    };
+    
+	}
+
 	res.status(200).json({ message: 'This works just fine!' });
 }
