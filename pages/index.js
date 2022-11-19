8921d8a03a76fbc420a4401ce7bb03958ff64b04
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-19 14:34:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
-import styles from '../styles/Home.module.css';
+import { useRef } from 'react';
 
 export default function Home() {
+	const emailRef = useRef();
+	const feedbackRef = useRef();
+
+	function submitHandler(event) {
+		event.preventDefault();
+		const emailEntered = emailRef.current.value;
+		const feedbackEntered = feedbackRef.current.value;
+
+
 	return (
-	
-			<h1 className={styles.title}>Welcome</h1>
+		<div>
+			<h1>HomePage</h1>
+			<form onSubmit={submitHandler}>
+				<div>
+					<label htmlFor="email">Your email </label>
+					<input type="email" ref={emailRef} id="email" />
+				</div>
+
+				<div>
+					<label htmlFor="feedback">Your feedback</label>
+					<textarea rows="5" ref={feedbackRef} id="feedback" />
+				</div>
+				<button>Send Feedback </button>
+			</form>
+		</div>
 	);
 }
