c3ec88f92a60a68c3757b3c72234f7b00965026c
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-20 22:37:08 +0100

    Update [feedbackID].js

--- pages/api/[feedbackID].js
+++ pages/api/[feedbackID].js
@@ -1,9 +1,11 @@
 import { filePath, extractFeedback } from './../../utils/helper';
 
 export default function handler(req, res) {
-	const feedbackFile = filePath();
+    const feedbackFile = filePath();
+    
 	const data = extractFeedback(feedbackFile);
 	const feedbackID = req.query.feedbackID;
-	const selectedFeedback = data.find((feedback) => feedback.id === feedbackID);
+    const selectedFeedback = data.find((feedback) => feedback.id === feedbackID);
+    
 	res.status(200).json({ feedback: selectedFeedback });
 }
